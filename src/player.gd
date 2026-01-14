2b863e9226b90e9c5dfad39b3f839b44a67a8344
GarlicLoaf <garic15@protonmail.com>
2026-01-14 15:36:28 +0100

    Added readme

--- src/player.gd
+++ src/player.gd
@@ -40,7 +40,7 @@ func _physics_process(delta: float) -> void:
 			velocity += get_gravity() * delta
 
 		# Jump
-		if Input.is_action_just_pressed("space") and is_on_floor():
+		if Input.is_action_just_pressed("space") and not is_grabbing and is_on_floor():
 			velocity.y = JUMP_VEL
 
 	# Movement
